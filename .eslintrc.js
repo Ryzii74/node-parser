9deba79a634cf7d88b2844d7baac7408004d20eb
Ryzii74 <Ryzii@yandex.ru>
2017-03-24 23:18:19 +0300

    + add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        sourceType: 'module'
+    },
+    extends: 'airbnb-base',
+    'rules': {
+        "no-plusplus": 0,
+        "arrow-parens": 0,
+        "indent": ["error", 4],
+        "no-param-reassign": 0,
+        "no-shadow": 0,
+        "no-underscore-dangle": 0,
+        "no-console": 0,
+
+        // только на клиенте пока нет нормального импорта
+        "no-unused-vars": 0,
+        "no-undef": 0
+    }
+};
