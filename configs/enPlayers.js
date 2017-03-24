58c64a1aadb7ccdcad2e77787400338109c59c70
Ryzii74 <Ryzii@yandex.ru>
2017-03-24 22:43:14 +0300

    + add test configs for en players

---
+++ configs/enPlayers.js
@@ -0,0 +1,21 @@
+module.exports = {
+    getter : {
+        type : 'list',
+        urlList : [
+            'http://cp.en.cx/UserList.aspx?mode=full'
+        ],
+        pageStructure : {
+            element : '#MainForm table:eq(1) tr',
+            fields : [
+                {
+                    name : 'id',
+                    selector : 'td:eq(1)'
+                }
+            ]
+        }
+    },
+    setter : {
+        type : 'mongo',
+        file : 'enplayers'
+    }
+};
